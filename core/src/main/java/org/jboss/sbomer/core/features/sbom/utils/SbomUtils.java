7fb9a5a1d041d0547c7774ecbd2e26c5c8526452
David Walluck <5445917+dwalluck@users.noreply.github.com>
2026-09-23 13:15:11 -0400

    fix: fix error message when extracting version from generic PURL (#1861)

--- core/src/main/java/org/jboss/sbomer/core/features/sbom/utils/SbomUtils.java
+++ core/src/main/java/org/jboss/sbomer/core/features/sbom/utils/SbomUtils.java
@@ -2125,7 +2125,7 @@ public class SbomUtils {
                 }
             }
         } catch (MalformedPackageURLException e) {
-            log.warn("Can't extract version from generic purl with error: {}", e);
+            log.warn("Can't extract version from generic purl: {}", e.getMessage());
         }
     }
 
